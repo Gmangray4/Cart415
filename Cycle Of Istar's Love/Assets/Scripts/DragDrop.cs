33c53e0dad9e4a42056dcdae1e0b39a939d48cab
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2020-09-20 18:46:03 -0400

    Drag and Drop added

---
+++ Cycle Of Istar's Love/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
+//Reference
+// https://oxmond.com/how-to-drag-and-drop-a-2d-object/
+// https://www.youtube.com/watch?v=55TBhlOt_U8&t=15s&ab_channel=OXMONDTutorials
