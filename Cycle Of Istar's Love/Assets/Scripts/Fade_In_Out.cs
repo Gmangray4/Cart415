2f04f6f843c7c74117a486e3bea0cefa7280fd44
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2020-09-23 20:13:57 -0400

    Update Bulid

---
+++ Cycle Of Istar's Love/Assets/Scripts/Fade_In_Out.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using System.Diagnostics;
+
+
+public class Fade_In_Out : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Transform img;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click");
+        img.transform.position = respawnPoint.transform.position;
+    }
+
+ 
+}
